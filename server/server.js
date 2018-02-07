fb059ea30f97a8717f68c359c209863c7dee9501
Valkora <vcorostinscaia@mitocgroup.com>
2018-02-07 18:10:53 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -29,9 +30,26 @@ app.get('/todos', (req, res) => {
   })
 });
 
+//GET
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(! ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(! todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(404).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log("Started on port 3000");
 });
-
 
 module.exports = {app};
