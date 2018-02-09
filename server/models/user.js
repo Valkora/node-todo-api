33b540a570193898c5ba2fea8ee7da8ace76b574
Valkora <vcorostinscaia@mitocgroup.com>
2018-02-09 18:32:29 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -68,8 +68,28 @@ UserSchema.statics.findByToken = function (token) {
   });
 }
 
-var User = mongoose.model('User', UserSchema);
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User =this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user){
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) =>{
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res){
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 UserSchema.pre('save', function(next){
   var user =this;
 
